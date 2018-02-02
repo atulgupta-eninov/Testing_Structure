b0bfdb35c7cf0bad78f8e9e89381040a48248aca
atulgupta-eninov <36026399+atulgupta-eninov@users.noreply.github.com>
2018-02-02 10:38:11 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -2,11 +2,11 @@
 
 filename="bundle-1-$1" # Name of binary file.
 
-file="test.c"  # C file.
+file="Test.c"  # C file.
 
 if [ -f $file ]
 then
-	gcc test.c -o $filename
+	gcc Test.c -o $filename
 	mv $filename $JENKINS_HOME/userContent/
 	echo "$filename is move in $JENKINS_HOME"
 	exit 0
